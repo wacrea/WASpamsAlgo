ebebdf7d94819dc57a60c51f97dfbc57135ab329
root <root@aiststudies-srv1.localhost>
2013-01-05 19:59:05 +0100

    Count & Akimset class call

--- algo.php
+++ algo.php
@@ -23,14 +23,20 @@
 	$select = $connection->query("SELECT * FROM wp_comments");
 	$comments = $select->fetchAll(PDO::FETCH_OBJ);
 	
-	echo count($comments)." rows fetched \n";
+	$total = count($comments);
+	echo $total." rows fetched \n";
+
+	$i = 0;
+
+	$APIKey = '5adeec31c278';
+	$BlogURL = 'http://bloglancome.isphers.com'
+
+	$Akismet = new Akismet($BlogURL, $APIKey);
 
 	foreach($comments AS $comment)
 	{
-		$APIKey = 'your Akismet API key';
-		$BlogURL = 'http://wordpress blog url';
+		$i++;
 
-		$Akismet = new Akismet($BlogURL ,$APIKey);
 		$Akismet->setCommentAuthor($comment->comment_author);
 		$Akismet->setCommentAuthorEmail($comment->comment_author_email);
 		$Akismet->setCommentAuthorURL($comment->comment_author_url);
@@ -40,10 +46,10 @@
 		{
 			$connection->query("DELETE FROM wp_comments WHERE comment_ID = '".$comment->comment_ID."'");
 			$connection->query("DELETE FROM wp_commentmeta WHERE comment_id = '".$comment->comment_ID."'");
-			echo "SPAM \n";
+			echo $i."/".$total." - SPAM \n";
 		}
 		else
 		{
-			echo "ok\n";
+			echo $i."/".$total." - ok\n";
 		}
 	}
